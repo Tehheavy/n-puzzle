452ca6b021183c224c784f438f2ab9b8b94ee2dd
Alex Dushevski <39350701+Tehheavy@users.noreply.github.com>
2020-05-10 04:01:22 +0300

    Updates

---
+++ precache-manifest.2ed8a7ffce9d0539d31c8024c2f17804.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5181d2c8ae0d4f06f965b736f3d12cb7",
+    "url": "/n-puzzle/index.html"
+  },
+  {
+    "revision": "be49995f9c6d130c0f69",
+    "url": "/n-puzzle/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3508dd12663b2042ef7f",
+    "url": "/n-puzzle/static/js/2.0f47ca7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/n-puzzle/static/js/2.0f47ca7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be49995f9c6d130c0f69",
+    "url": "/n-puzzle/static/js/main.8e17db6e.chunk.js"
+  },
+  {
+    "revision": "c87f1bc21a994fb6336b",
+    "url": "/n-puzzle/static/js/runtime-main.10fb5df4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/n-puzzle/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/n-puzzle/precache-manifest.2ed8a7ffce9d0539d31c8024c2f17804.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/n-puzzle/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
